a76e65363eede4af9cfe28773949a8c18a8dcc15
ansarsufiyana <ansarali106@gmail.com>
2022-05-19 17:07:05 +0500

    todays study and code wrok creating a weather app

---
+++ index.js
@@ -0,0 +1,27 @@
+let weather = {
+  apikey: "45fe15572d2ef49cfe33e837652bff53",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then((Response) => Response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function(data){
+      const { name } = data ;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      console.log(name,icon,description,temp,humidity,speed);
+      document.querySelector(".city").innerHTML = ' Weather in ' + name; 
+      document.querySelector(".icon").src = `http://openweathermap.org/img/w/${icon}.png`
+      document.querySelector(".description").innerHTML = description;
+      document.querySelector(".temp").innerHTML = temp + "°C";
+      document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+}
+};
+ 
