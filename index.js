a40a3acbbad973ac5fd2c983625a41c4b8beac85
ansarsufiyana <ansarali106@gmail.com>
2022-05-20 12:20:24 +0500

    weather app completed

--- index.js
+++ index.js
@@ -10,30 +10,39 @@ let weather = {
       .then((Response) => Response.json())
       .then((data) => this.displayWeather(data));
   },
-  displayWeather: function(data){
-      const { name } = data ;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
-      console.log(name,icon,description,temp,humidity,speed);
-      document.querySelector(".city").innerHTML = ' Weather in ' + name; 
-      document.querySelector(".icon").src = `http://openweathermap.org/img/w/${icon}.png`
-      document.querySelector(".description").innerHTML = description;
-      document.querySelector(".temp").innerHTML = temp + "°C";
-      document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
-},
-search: function () {
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerHTML = " Weather in " + name;
+    document.querySelector(
+      ".icon"
+    ).src = `http://openweathermap.org/img/w/${icon}.png`;
+    document.querySelector(".description").innerHTML = description;
+    document.querySelector(".temp").innerHTML = temp + "°C";
+    document.querySelector(".humidity").innerHTML =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/random/?" + name + "')"
+  },
+  search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
-}
+  },
 };
- 
-document.querySelector(".search button").addEventListener("click", function (){
-    weather.search();
-})
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event){
-  if (event.key == "enter"){
-    weather.search();
-  }
-})
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("pakistan");
